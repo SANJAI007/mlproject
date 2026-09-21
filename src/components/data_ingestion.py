030c473b10dd4dbf1f3faee28efef6fc60118cc6
Sanjai007 <sanjaikumaar007@gmail.com>
2026-09-21 11:52:39 +0530

    Data Tranformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -25,6 +27,13 @@ class DataIngestion:
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             os.makedirs(os.path.dirname(self.ingestion_config.test_data_path), exist_ok=True)
+            df.rename(
+                columns={"race/ethnicity": "race_or_ethnicity", 
+                "parental level of education": "parental_level_of_education",
+                "test preparation course":"test_preparation_course",
+                "math score": "math_score",
+                "reading score": "reading_score",
+                "writing score": "writing_score"}, inplace=True)
 
             df.to_csv(self.ingestion_config.raw_data_path, index =False, header=True)
 
@@ -46,5 +55,8 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
 
